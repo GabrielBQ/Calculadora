512aa2a3c8fb222269b00e6ff54ffe1f35336b83
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-10-23 13:51:19 +0200

    Añadido el JS de la calculadora

---
+++ index.js
@@ -0,0 +1,15 @@
+//result=casilla de resultados
+const result = document.querySelector(".results");
+//FUNCIÓN DEL BOTÓN C
+const buttonC = document.querySelector(".botonC");
+buttonC.addEventListener("click", eliminar);
+
+function eliminar(evento1) {
+  const eliminado = erase(evento1.target.innerText);
+  //Donde quiero que borre-->(en results)
+  result.innerText = eliminado;
+}
+
+function erase(n) {
+  return (n = 0);
+}
