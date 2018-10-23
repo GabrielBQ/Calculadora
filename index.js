7567f81e0938a526312ec5361f6242cd31dbb6db
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-10-23 14:44:32 +0200

    Añadida Función onCLick para todos los botones

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-//result=casilla de resultados
-const result = document.querySelector(".results");
-//FUNCIÓN DEL BOTÓN C
-const buttonC = document.querySelector(".botonC");
-buttonC.addEventListener("click", eliminar);
+const button = document.querySelector(".valor");
+button.addEventListener("click", onClick);
 
-function eliminar(evento1) {
-  const eliminado = erase(evento1.target.innerText);
-  //Donde quiero que borre-->(en results)
-  result.innerText = eliminado;
-}
+function onClick(evento) {
+  const cliquear = evento.target.innerText;
+  result.innerText = cliquear;
 
-function erase(n) {
-  return (n = 0);
+  //result=casilla de resultados
+  const result = document.querySelector(".results");
+  //FUNCIÓN DEL BOTÓN C
+  const buttonC = document.querySelector(".botonC");
+  buttonC.addEventListener("click", eliminar);
+
+  function eliminar(evento1) {
+    const eliminado = erase(evento1.target.innerText);
+    //Donde quiero que borre-->(en results)
+    result.innerText = eliminado;
+  }
+
+  function erase(n) {
+    return (n = 0);
+  }
 }
