453535b3e60a3234bd36dca5e9cfebc8375aee4f
Gabriel B Quintáns <gabrielbarreiroquintans@gmail.com>
2018-10-24 08:40:38 +0200

    Funciona a través del switch la tecla C

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-const button = document.querySelector(".valor");
-button.addEventListener("click", onClick);
+const result = document.querySelector(".results");
+
+const container = document.querySelector(".valor");
+container.addEventListener("click", onClick);
 
 function onClick(evento) {
   const cliquear = evento.target.innerText;
-  result.innerText = cliquear;
+  console.log(`Clickado el ${cliquear}`);
 
-  //result=casilla de resultados
-  const result = document.querySelector(".results");
-  //FUNCIÓN DEL BOTÓN C
-  const buttonC = document.querySelector(".botonC");
-  buttonC.addEventListener("click", eliminar);
-
-  function eliminar(evento1) {
-    const eliminado = erase(evento1.target.innerText);
-    //Donde quiero que borre-->(en results)
-    result.innerText = eliminado;
+  switch (cliquear) {
+    case "C":
+      eliminar();
+      break;
   }
+}
 
-  function erase(n) {
-    return (n = 0);
-  }
+function eliminar() {
+  result.innerText = 0;
 }
